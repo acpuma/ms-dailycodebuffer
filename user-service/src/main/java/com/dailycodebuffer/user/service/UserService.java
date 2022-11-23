5bbdc49e40e709f52754dee63e8a20721de2d469
Cuma Cakmak <cumacakmak@gmail.com>
2022-11-23 23:37:35 +0300

    Add Service Registry

--- user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
+++ user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -27,7 +27,7 @@ public class UserService {
         log.info("Inside getUserWithDepartment in UserService");
         ResponseTemplateVO vo = new ResponseTemplateVO();
         User user = userRepository.findByUserId(userId);
-        Department department = restTemplate.getForObject("http://localhost:9001/departments/"
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/"
                 + user.getDepartmentId(), Department.class);
         vo.setUser(user);
         vo.setDepartment(department);
